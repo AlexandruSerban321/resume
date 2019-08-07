04cd9c67ddd521888d4884b9d4def0e488fdbdde
AlexandruSerban321 <alexandruserban160@gmail.com>
2019-08-07 23:36:25 +0300

    post app is done

---
+++ post/views.py
@@ -0,0 +1,41 @@
+from django.shortcuts import render
+from django.views.generic import CreateView, UpdateView, DeleteView, DetailView
+from .models import Post
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+
+# Create your views here.
+
+class PostCreateView(LoginRequiredMixin ,CreateView):
+    model = Post
+    fields = ['title','content']
+    
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView(UserPassesTestMixin ,LoginRequiredMixin ,UpdateView):
+    model = Post
+    fields = ['title','content']
+    
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+class PostDeleteView(UserPassesTestMixin ,LoginRequiredMixin ,DeleteView):
+    model = Post
+    success_url = 'home'
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False 
+
+class PostDetailView(DetailView):
+    model = Post
