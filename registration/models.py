3c18633d2e8028693884ba80516a76e1f02d021c
AlexandruSerban321 <alexandruserban160@gmail.com>
2019-08-07 23:34:49 +0300

    rezolved error of user profiles not beeing created on account registration

--- registration/models.py
+++ registration/models.py
@@ -1,16 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
 from PIL import Image
+from django.db.models.signals import post_save
 
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+    image = models.ImageField(default='default.png', upload_to='profile_pics')
 
     def __str__(self):
         return f'{self.user.username} Profile'
 
-    def save(self):
+    def save(self, *args, **kwargs):
         super().save()
 
         img = Image.open(self.image.path)
@@ -19,3 +20,10 @@ class Profile(models.Model):
             output_size = (300, 300)
             img.thumbnail(output_size)
             img.save(self.image.path)
+
+def create_user_profile(sender, **kwargs):
+    if kwargs['created']:
+        user_profile = Profile.objects.create(user=kwargs['instance'])
+
+
+post_save.connect(create_user_profile, sender=User)
