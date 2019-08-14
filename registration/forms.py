fdf49732ef0eac9d3f79b1ca2156c197a5e21f96
AlexandruSerban321 <alexandruserban160@gmail.com>
2019-08-14 16:22:01 +0300

    email field on registration is now unique

--- registration/forms.py
+++ registration/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.contrib.auth.models import User
+User._meta.get_field('email')._unique = True
 from django.contrib.auth.forms import UserCreationForm
 from .models import Profile
 
