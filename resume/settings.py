02a30295b9ed7902c00e4ad94d99277713c089e1
AlexandruSerban321 <alexandruserban160@gmail.com>
2019-10-24 22:24:47 +0300

    added python-dotenv as a replacement for default os variables

--- resume/settings.py
+++ resume/settings.py
@@ -9,7 +9,6 @@ https://docs.djangoproject.com/en/2.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
-
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -20,7 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -89,7 +88,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'alexandruserban0$resume',
         'USER': 'alexandruserban0',
-        'PASSWORD': os.environ.get('DATA_BASE_PASSWORD'),
+        'PASSWORD': os.getenv('DATA_BASE_PASSWORD'),
         'HOST': 'alexandruserban09080.mysql.pythonanywhere-services.com',
     }
 }
@@ -143,5 +142,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
--- resume/wsgi.py
+++ resume/wsgi.py
@@ -1,4 +1,6 @@
 import os
 from django.core.wsgi import get_wsgi_application
+from dotenv import load_dotenv
+load_dotenv()
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'resume.local_settings')
 application = get_wsgi_application()
