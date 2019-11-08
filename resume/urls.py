882a441f340c14330f1b0ec510f70060e2f03da5
AlexandruSerban321 <alexandruserban160@gmail.com>
2019-11-08 11:56:54 +0200

    removed unnecesary condition for media root

--- resume/urls.py
+++ resume/urls.py
@@ -23,6 +23,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', Home_view, name='home'),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
