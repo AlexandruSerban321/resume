9b618840e06c8881c247c631f939ae287abd480e
AlexandruSerban321 <alexandruserban160@gmail.com>
2019-11-29 19:49:05 +0200

    changed admin url path to santa because chrismas is coming

--- resume/urls.py
+++ resume/urls.py
@@ -35,7 +35,7 @@ urlpatterns = [
         template_name='password_reset_confirm.html'), name='password_reset_confirm'),
     path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(
         template_name='password_reset_complete.html'), name='password_reset_complete'),
-    path('admin/', admin.site.urls),
+    path('santa/', admin.site.urls),
     path('', Home_view, name='home'),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
